2ef9b02d7aff2bf85587db29ac4012b05dc6fecf
Polina-Krivdina <119794032+Polina-Krivdina@users.noreply.github.com>
2024-01-30 11:18:09 -0800

    Update lesson_2_task_9.py

--- lesson_2_task_9.py
+++ lesson_2_task_9.py
@@ -1,5 +1,8 @@
 var_1 = 37
 var_2 = 99
-var_1 = 99
-var_2 = 37
-print(f" Значение var_1:  {var_1}, Значение var_2:  {var_2}")
+
+var_1, var_2 = var_2, var_1
+
+print("Обновленные переменные:")
+print(f"var_1 = {var_1}")
+print(f"var_2 = {var_2}")
